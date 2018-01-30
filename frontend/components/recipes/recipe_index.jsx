4cd2a9785c3033ca935e136bc1cd7d9e6d748f5b
Joe Fiume <joefiume88@gmail.com>
2018-01-29 21:38:13 -0800

    added a recipe index

--- frontend/components/recipes/recipe_index.jsx
+++ frontend/components/recipes/recipe_index.jsx
@@ -1,2 +1,33 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import RecipeIndexItemContainer from './recipe_index_item_container';
+
+class RecipeIndex extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchRecipes();
+  }
+
+  render() {
+    if (Object.keys(this.props.recipes).length > 0) {
+      const { recipes } = this.props;
+
+      return (
+        <section className="all-recipes">
+          <ul className="recipes">
+            {Object.values(recipes).map(food => <RecipeIndexItemContainer key={food.id} recipe={food} />)}
+          </ul>
+        </section>
+      );
+    } else {
+      return (
+        <div>loading</div>
+      );
+    }
+  }
+}
+
+export default RecipeIndex;

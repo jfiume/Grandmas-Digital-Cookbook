7b773b8670572998c861d63917a5c81e154aaf30
Joe Fiume <joefiume88@gmail.com>
2018-01-29 20:39:24 -0800

    added store

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter,
+} from 'react-router-dom'
 
 const App = () => (
-  <div>Hello world</div>
+  <div>
+    <Route path="/" />
+  </div>
 );
 
 export default App;

3bd3863770d1eae34bb1adc3072d3d142f498943
Joe Fiume <joefiume88@gmail.com>
2018-02-13 18:06:23 -0800

    added renderprep and renderdirections methods

--- frontend/components/recipes/recipe_show.jsx
+++ frontend/components/recipes/recipe_show.jsx
@@ -11,6 +11,10 @@ export const Directions = styled.article`
 
 `;
 
+export const Prep = styled.article`
+
+`;
+
 export const PartentSection = styled.section`
 
 `;
@@ -22,10 +26,24 @@ export const IngredientsList = ({ ingredient }) => {
   )
 }
 
+export const PrepList = ({ prep }) => {
+  if (prep.length !== 0) {
+    return (
+      <li>{prep}</li>
+    )
+  }
+}
+
+export const DirectionsList = ({ directions }) => (
+    <li>{directions}</li>
+);
+
 class RecipeShow extends Component {
   constructor(props) {
     super(props);
     this.renderIngredients = this.renderIngredients.bind(this);
+    this.renderPrep = this.renderPrep.bind(this);
+    this.renderDirections = this.renderDirections.bind(this);
   }
 
   componentDidMount() {
@@ -50,13 +68,38 @@ class RecipeShow extends Component {
     );
   }
 
+  renderDirections() {
+    const directions = this.props.currentRecipe.directions;
+    return (
+      <div>
+        <h2>Directions</h2>
+        <ul>
+          {directions.map(direction => <IngredientsList key={direction} direction={direction} /> )}
+        </ul>
+      </div>
+    );
+  }
+
+  renderPrep() {
+    const prep = this.props.currentRecipe.prep;
+    return (
+      <div>
+        <h2>Prep</h2>
+        <ul>
+          {prep.map(prepSteps => <PrepList key={prepSteps} prep={prepSteps} /> )}
+        </ul>
+      </div>
+    );
+  }
+
   render() {
     if (this.props.recipeId === parseInt(this.props.match.params.id)) {
       const recipe = this.props.currentRecipe;
       return (
         <PartentSection>
           <Ingredients>{this.renderIngredients()}</Ingredients>
-          <Directions>{recipe.directions}</Directions>
+          <Prep>{this.renderPrep()}</Prep>
+          <Directions>{this.renderDirections()}</Directions>
         </PartentSection>
       );
     } else {

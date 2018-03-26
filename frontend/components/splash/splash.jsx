d579863b637a6244b029920b6f86cc09badcbc8f
Joe Fiume <joefiume88@gmail.com>
2018-03-26 14:49:24 -0700

    added links to the splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,6 +6,12 @@ const host = window.location.host;
 const Welcome = styled.h1`
   margin-top: 80px;
 `;
+const LinkStyle = {
+  textDecoration: 'none',
+  color: 'black',
+  border: 'solid red 5px',
+  backgroundColor: 'red'
+}
 
 class Splash extends Component {
   constructor(props) {
@@ -16,6 +22,7 @@ class Splash extends Component {
     return (
       <section>
         <Welcome>Welcome to Grandma's Digital Cookbook</Welcome>
+        <Link to="/recipes" style={LinkStyle}>Home Page</Link>
         {/* <a href="https://grandmas-digital-cookbook.herokuapp.com/users/sign_in"> Sign In</a> */}
       </section>
     )
